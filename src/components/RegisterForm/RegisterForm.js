f69e811ca13ab07017c29af2ac6b0332b15e8e87
Nick Evans <nickevans20@outlook.com>
2020-09-08 15:29:05 -0500

    Setup 'RegisterForm.js' with registration inputs to create an account, adding gender specification criteria later and it will not be required to submit form

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -4,6 +4,8 @@ import mapStoreToProps from '../../redux/mapStoreToProps';
 
 class RegisterForm extends Component {
   state = {
+    firstName: '',
+    lastName: '',
     username: '',
     password: '',
   };
@@ -14,6 +16,8 @@ class RegisterForm extends Component {
     this.props.dispatch({
       type: 'REGISTER',
       payload: {
+        firstName: this.state.first,
+        lastName: this.state.lastName,
         username: this.state.username,
         password: this.state.password,
       },
@@ -29,16 +33,45 @@ class RegisterForm extends Component {
   render() {
     return (
       <form className="formPanel" onSubmit={this.registerUser}>
-        <h2>Register User</h2>
+        <h2>Create iCloset Account</h2>
         {this.props.store.errors.registrationMessage && (
           <h3 className="alert" role="alert">
             {this.props.store.errors.registrationMessage}
           </h3>
         )}
         <div>
+          <label htmlFor="first-name">
+            First Name:
+            <input
+              placeholder="First Name"
+              type="text"
+              name="first-name"
+              value={this.state.firstName}
+              required
+              onChange={this.handleInputChangeFor('firstName')}
+            />
+          </label>
+        </div>
+    
+        <div>
+          <label htmlFor="last-name">
+            Last Name:
+            <input
+              placeholder="Last Name"
+              type="text"
+              name="last-name"
+              value={this.state.lastName}
+              required
+              onChange={this.handleInputChangeFor('lastName')}
+            />
+          </label>
+        </div>
+
+        <div>
           <label htmlFor="username">
             Username:
             <input
+            placeholder="Username"
               type="text"
               name="username"
               value={this.state.username}
@@ -47,10 +80,12 @@ class RegisterForm extends Component {
             />
           </label>
         </div>
+
         <div>
           <label htmlFor="password">
             Password:
             <input
+              placeholder="Password"
               type="password"
               name="password"
               value={this.state.password}
@@ -59,12 +94,14 @@ class RegisterForm extends Component {
             />
           </label>
         </div>
+        {/* figure out what to do with button when done with first name and last name input set */}
         <div>
-          <input className="btn" type="submit" name="submit" value="Register" />
+          <input className="btn" type="submit" name="submit" value="Create Account" />
         </div>
       </form>
     );
   }
 }
+
 
 export default connect(mapStoreToProps)(RegisterForm);
--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -21,7 +21,7 @@ class RegisterPage extends Component {
             type="button"
             className="btn btn_asLink"
             onClick={() => {
-              this.props.history.push('/login');
+              this.props.history.push('/login'); // pushes user to login page after completing Create Account form
             }}
           >
             Login
